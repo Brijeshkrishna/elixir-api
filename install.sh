c5481260b38e8b25a3030eb9c7cc95358195e6e3
Brijesh <63464137+Brijeshkrishna@users.noreply.github.com>
2024-04-17 12:41:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ ollama pull  all-minilm
 ollama pull nomic-embed-text 
 ollama pull mxbai-embed-large 
 
+sudo docker run -p 6333:6333 -v embedings:/qdrant/storage   qdrant/qdrant&
 OLLAMA_HOST=0.0.0.0:2563 ollama serve
