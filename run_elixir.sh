9f8c77ff60cd9593dd6b3ff63d10edb0b3ccdd8f
Brijesh <63464137+Brijeshkrishna@users.noreply.github.com>
2024-04-17 12:45:24 +0530

    Update run_elixir.sh

--- run_elixir.sh
+++ run_elixir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-poetry update
-ollama serve
-poetry run uvicorn elixir_api.main:app --reload
+sudo docker run -p 6333:6333 -v embedings:/qdrant/storage   qdrant/qdrant&
+OLLAMA_HOST=0.0.0.0:2563 ollama serve&
+poetry run python uvicorn elixir_api.main:app --host 0.0.0.0 --port 8000 --workers 1 
