3db14be05d5813e2841efc8a96b0e75b3bc20936
Brijesh <63464137+Brijeshkrishna@users.noreply.github.com>
2024-04-17 12:45:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 curl -fsSL https://ollama.com/install.sh | sh
 
 poetry update
-
-sudo docker run -p 6333:6333 -v embedings:/qdrant/storage   qdrant/qdrant&
-OLLAMA_HOST=0.0.0.0:2563 ollama serve&
-poetry run python uvicorn elixir_api.main:app --host 0.0.0.0 --port 8000 --workers 1 
