61709581386072355579c3931ed7ad3b516431ef
Brijesh <63464137+Brijeshkrishna@users.noreply.github.com>
2024-04-17 12:43:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ ollama pull  all-minilm
 ollama pull nomic-embed-text 
 ollama pull mxbai-embed-large 
 
+poetry update
+
 sudo docker run -p 6333:6333 -v embedings:/qdrant/storage   qdrant/qdrant&
-OLLAMA_HOST=0.0.0.0:2563 ollama serve
+OLLAMA_HOST=0.0.0.0:2563 ollama serve&
+poetry run python uvicorn elixir_api.main:app --host 0.0.0.0 --port 8000 --workers 1 
